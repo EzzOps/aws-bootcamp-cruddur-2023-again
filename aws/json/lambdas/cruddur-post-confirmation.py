55c891270f287547ed003a29adbe766873b8c3e0
Ezz Ashraf AboAlhamed <73319030+ezz444@users.noreply.github.com>
2023-04-24 10:26:53 +0200

    implement create activity

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -8,30 +8,36 @@ def lambda_handler(event, context):
     print("user Atributies")
     print(user)
 
-    user_display_name= user['name']
-    user_email= user['email']
-    user_handle= user["preferred_username"]
-    cognito_user_id= user['sub']
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    user_cognito_id    = user['sub']
     try:
+      print('entered-try')
+      sql = f"""
+         INSERT INTO public.users (
+          display_name, 
+          email,
+          handle, 
+          cognito_user_id
+          ) 
+        VALUES(%s,%s,%s,%s)
+      """
+      print('SQL Statement ----')
+      print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      sql= f"""
-        INSERT INTO public.users (display_name, handle, email, cognito_user_id)
-          VALUES(
-            '{user_display_name}',
-            '{user_email}',
-            '{user_handle}',
-            '{cognito_user_id}'
-              )
-        """
-      print("SQL-----")
-      print(sql)
-      cur.execute (sql)        
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql,*params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
       print(error)
-        
     finally:
       if conn is not None:
         cur.close()
