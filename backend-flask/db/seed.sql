ccab0f98151d4fe96133a474b5b8c8a2ccf695eb
ezz444 <3202111112@alexu.edu.eg>
2023-05-06 22:51:44 +0000

    update sql files

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@example.co', 'andrewbrown' ,'c4931da0-12de-471a-8b5b-ba82618834ee'),
-  ('Andrew Bayko', 'bayko@example.co', 'bayko' ,'d1d3fd37-557e-4358-8fc9-399128ad9039'),
-  ('ezz', 'ezzashraf444@gmail.com', 'EzzOps' ,'bd0d5a5f-3af7-4c3e-8dfc-1735f46df8af');
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
