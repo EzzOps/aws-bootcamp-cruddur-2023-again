37518dfdcc564dfc0e4cfd34168656a19083682e
ezz444 <3202111112@alexu.edu.eg>
2023-05-12 00:55:47 +0000

    fixes related to home/message pages

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,15 +1,24 @@
 from datetime import datetime, timedelta, timezone
-from opentelemetry import trace
+from opentelemetry import trace # uses only opentelemetry-api in requirements.txt
+
 from lib.db import db
 
-#tracer = trace.get_tracer("home.activities")
+# tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
   def run(cognito_user_id=None):
-    #logger.info("HomeActivities")
-    #with tracer.start_as_current_span("home-activites-mock-data"):
-    #  span = trace.get_current_span()
-    #  now = datetime.now(timezone.utc).astimezone()
-    #  span.set_attribute("app.now", now.isoformat())
-    sql = db.template('activities','home')
-    results = db.query_array_json(sql)
+  # def run(logger):
+  #   logger.info("Logs from HomeActivities.py")
+  # LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
+  # with tracer.start_as_current_span("mock-data-home-activities"): # This line creates spans
+  #   span = trace.get_current_span()           # this and the next lines add attributes to spans
+  #   now = datetime.now(timezone.utc).astimezone()
+  #   span.set_attribute("app.now", now.isoformat())
+  #   span.set_attribute("app.env", "local machine")
+    # span.set_attribute("app.result_length", len(results))
+    
+    sql = db.template('activities', 'home')
+    results = db.query_json_array(sql)
+    print(f"sql statement: {sql}\n")
+
     return results
